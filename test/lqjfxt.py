b25d042ab26f382b192e487caad418b2d7d110c0
liunxroot <48301758+liunxroot@users.noreply.github.com>
2020-09-05 00:07:31 +0800

    Update lqjfxt.py
    第二次修改

--- test/lqjfxt.py
+++ test/lqjfxt.py
@@ -2,43 +2,42 @@
 huren = []
 zhongguo = []
 print('****************NBA比赛记分系统****************')
-
 def fenshu():
-    dui = input('请选择得分球队：湖人队【1】 中国队【2】比赛结果按g\n请输入：')
-
+    dui = input('请选择得分球队：湖人队【1】 中国队【2】比赛结果按：e\n请输入：')
     if dui == '1':
         while 1:
             defen = input("您选择的是【湖人队】请输入得分：\n得分(输入q退出)：")
             if defen == 'q':
                 fenshu()
-            else:
+            elif defen.isdigit():
                 defen = int(defen)
                 huren.append(defen)
+            else:
+                print('输入有误，请输入数字！')
     elif dui == '2':
         while 1:
             defen = input("您选择的是【中国队】请输入得分：\n得分(输入q退出)：")
             if defen == 'q':
                 fenshu()
-            else:
-                defen =int(defen)
+            elif defen.isdigit():
+                defen = int(defen)
                 zhongguo.append(defen)
-    elif dui == 'g':
+            else:
+                print('输入有误，请输入数字！')
+    elif dui == 'e':
         print('湖人队得分：',sum(huren))
         print('中国队得分：',sum(zhongguo))
         if sum(huren) > sum(zhongguo):
             print('冠军是：湖人队')
+
         elif sum(huren) == sum(zhongguo):
             print('比赛为平局！')
+
         else:
-                #sum(huren) < sum(zhongguo):
             print('冠军是：中国队\n比赛结束！')
-
-            #print('比赛结束！')
-
-
     else:
         print('输入有误，请重新输入！')
         fenshu()
-
 fenshu()
+print('****************NBA比赛记分系统****************')
 
